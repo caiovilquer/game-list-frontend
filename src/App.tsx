0951f3db80a9ce897c595e5acfdef4e84a97714a
caiovilquer <caiovcarvalho337@gmail.com>
2025-08-19 21:36:37 -0300

    feat: enhance styling with Tailwind CSS, add animations, and define color variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GameList } from "./pages/GameList";
 import { GameDetail } from "./pages/GameDetail";
 import { GameForm } from "./pages/GameForm";
-import { RankingPage } from "./pages/GenrePage";
 import { GamesPage } from "./pages/GamesPage";
-import "./assets/styles/global.css";
+import { HowItWorks } from "./pages/HowItWorks";
+import { RankingPage } from "./pages/GenrePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<GamesPage />} />
-        <Route path="/games/:id" element={<GameDetail />} />
-        <Route path="/new" element={<GameForm />} />
-        <Route path="/generos/:id" element={<RankingPage />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-900 to-black text-white">
+        <Header />
+        <main className="flex-grow container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<GamesPage />} />
+            <Route path="/games/:id" element={<GameDetail />} />
+            <Route path="/new" element={<GameForm />} />
+            <Route path="/how-it-works" element={<HowItWorks />} />
+            <Route path="/generos/:id" element={<RankingPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
